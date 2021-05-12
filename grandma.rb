16b1ef0096c5eac3cd29370876c6682f59297f0c
Eric Ontiveros <ericontiveros123@gmail.com>
2021-05-12 17:56:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ speak_to_grandma = gets.chomp
 if speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU PUMPKIN!"
 elsif speak_to_grandma == "Hows the weather?"
-  puts "Huh?! Speak up, sonny!".uppercase
+  puts "Huh?! Speak up, sonny!"
 else
-  puts "No, not since 1938!".uppercase
+  puts "No, not since 1938!"
 end
