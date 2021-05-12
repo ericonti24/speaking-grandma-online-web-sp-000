a0a880d4807887b0f8f84b965e36e5c18d228287
Eric Ontiveros <ericontiveros123@gmail.com>
2021-05-12 21:32:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,12 +14,14 @@
 
 #speak_to_grandma = gets.chomp
 
+
+
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU PUMPKIN!"
-  elsif phrase != phrase.uppercase
-    return "HUH, SPEAK UP, SONNY!"
-  else phrase == phrase.uppercase
-    return "NO, NOT SINCE 1938!"
-  end
+         if phrase == "I LOVE YOU GRANDMA!"
+				        "I LOVE YOU TOO PUMPKIN"
+				 elsif phrase != phrase.upcase
+				        "HUH!? SPEAK UP SONNY!"
+				 else phrase == phrase.upcase #if our argument of phrase is EQUAL TO phrase.upcase (uppercase) then its true!
+				        "NO, NOT SINCE 1938!"      #so grandma responds....
+				end
 end
